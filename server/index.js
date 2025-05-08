06a78123c96c116c736833a9eeb986ab8fb20962
[soumashree das] <[soumshreedas2004@gmail.com]>
2025-05-08 12:27:05 +0530

    need to deplo

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import JournalRouters from "./src/routes/journalEntry.routes.js";
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://deploy-mood-tracker.vercel.app');
+    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
