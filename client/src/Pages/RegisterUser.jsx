a510a89c5081e54fc77479e1331fd6c8752cea1a
[soumashree das] <[soumshreedas2004@gmail.com]>
2025-05-09 04:23:03 +0530

    deployed

--- client/src/Pages/RegisterUser.jsx
+++ client/src/Pages/RegisterUser.jsx
@@ -122,7 +122,7 @@ function RegisterUser() {
           id="name"
           autoComplete='off'
           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
         />
 
         <label htmlFor="email" className="block text-sm font-semibold text-gray-700 mb-1">Email</label>
@@ -133,7 +133,7 @@ function RegisterUser() {
           id="email"
           autoComplete='off'
           onChange={(e)=>setFormData({...formData,email:e.target.value})}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
         />
 
         <label htmlFor="password" className="block text-sm font-semibold text-gray-700 mb-1">Password</label>
@@ -144,7 +144,7 @@ function RegisterUser() {
           id="password"
           autoComplete='off'
           onChange={(e)=>setFormData({...formData,password:e.target.value})}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
         />
 
         <label htmlFor="phoneNumber" className="block text-sm font-semibold text-gray-700 mb-1">Phone Number</label>
@@ -156,7 +156,7 @@ function RegisterUser() {
           autoComplete='off'
           onChange={(e)=>setFormData({...formData,phoneNumber:e.target.value})}
           pattern="\d{10}"
-          className="w-full px-4 py-2 mb-6 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
         />
 
         <button 
